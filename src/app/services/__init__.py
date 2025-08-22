8cc54c58197610039fe8e1cfe6796ecc66aae235
Bhaven Kakade <thisisbhaven@gmail.com>
2025-08-22 18:00:12 +0530

    fix: Remove tenant_service imports from services/__init__.py
    - Removed deleted tenant_service references that were causing import errors
    - All 385 tests now passing

--- src/app/services/__init__.py
+++ src/app/services/__init__.py
@@ -4,7 +4,6 @@ from .base_service import BaseService
 from .cache_service import CacheService, cache_service
 from .network_service import NetworkService, network_service
 from .redis_consumer import RedisConfigConsumer
-from .tenant_service import TenantService, tenant_service
 
 __all__ = [
     "BaseService",
@@ -13,6 +12,4 @@ __all__ = [
     "NetworkService",
     "network_service",
     "RedisConfigConsumer",
-    "TenantService",
-    "tenant_service",
 ]
